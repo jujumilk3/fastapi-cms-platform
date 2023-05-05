5ca306712c69df33c90f77aefa9d92ee51f2db22
jujumilk3 <jujumilk3@gmail.com>
2023-05-06 00:07:23 +0900

    TEST: test util of create_user_and_get_jwt_reaction

--- tests/unit_tests/test_utils.py
+++ tests/unit_tests/test_utils.py
@@ -1,7 +1,26 @@
 from tests.utils.common import create_user_and_get_token
+from fastapi import status
 
 
-def test_crud_reaction(client, test_name):
+def test_create_user_and_get_jwt_reaction(client, test_name):
     # create 15 users and get tokens
     for i in range(15):
         assert "Bearer" in create_user_and_get_token(client, f"test_user_{test_name}_{i}")
+        # intended fail sign in
+        sign_in_response = client.post(
+            "/v1/auth/signin",
+            json={
+                "email": f"test_user_{test_name}_{i}",
+                "password": "wrong_password",
+            },
+        )
+        assert sign_in_response.status_code == status.HTTP_401_UNAUTHORIZED
+
+        sign_in_response = client.post(
+            "/v1/auth/signin",
+            json={
+                "email": f"test_user_{test_name}_{i}",
+                "password": "1234",
+            },
+        )
+        assert sign_in_response.status_code == status.HTTP_200_OK
